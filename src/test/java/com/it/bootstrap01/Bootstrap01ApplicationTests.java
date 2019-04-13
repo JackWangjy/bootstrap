ac6cd9e1d29dcf838238c409dbe06e759529f1a5
Administrator <Administrator@PC-20180223MDZW>
2019-04-13 14:36:48 +0800

    The learn of bootstrap

---
+++ src/test/java/com/it/bootstrap01/Bootstrap01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.it.bootstrap01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bootstrap01ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
